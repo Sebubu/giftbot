9a0da4bebc5fb6edc0a697ce975db1cff2c30e03
Severin Buhler <sevi_buehler@hotmail.com>
2017-09-16 01:29:36 +0200

    asdf

--- instarecom/views.py
+++ instarecom/views.py
@@ -1,7 +1,13 @@
 from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+import json
 
-# Create your views here.
+
+@csrf_exempt
 def recommendations(request):
+    print(request)
+    body = request.body.decode('utf-8')
+    request = json.loads(body, ensure_ascii=False)
     response = {
         'blaa': 'asdfasdf'
     }
