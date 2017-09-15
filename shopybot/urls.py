f77cd3c105de0f40d76b1bce4ff2d9a46cbf4d57
Severin Buhler <sevi_buehler@hotmail.com>
2017-09-16 00:24:41 +0200

    django update

--- shopybot/urls.py
+++ shopybot/urls.py
@@ -15,7 +15,9 @@ Including another URLconf
 """
 from django.conf.urls import url
 from django.contrib import admin
+from instarecom.views import recommendations
 
 urlpatterns = [
     url(r'^admin/', admin.site.urls),
+    url(r'^recommend/$', recommendations),
 ]
